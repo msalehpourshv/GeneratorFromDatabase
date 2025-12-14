c6d8b0d9b0abe7470539d6b98b8f554c187a765a
Mohammad Salehpour ShirinVatan <msalehpourshv@gmail.com>
2025-12-14 11:37:44 +0330

    Add Swagger support to Host API

--- Host/Program.cs
+++ Host/Program.cs
@@ -4,10 +4,15 @@ using SchemaReader;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<ISchemaReader, SchemaReaderService>();
 builder.Services.AddTransient<GeneratorService>();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
